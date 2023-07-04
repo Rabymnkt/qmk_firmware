8f4f7115b04dfd4a49f4c003be36dd1d07dc3883
Ryoma Minakata <rabyssm@gmail.com>
2023-07-05 01:06:31 +0900

    fix keymap.c

--- keyboards/planck/rev7/keymaps/rabymnkt/keymap.c
+++ keyboards/planck/rev7/keymaps/rabymnkt/keymap.c
@@ -1,5 +1,9 @@
 #include QMK_KEYBOARD_H
 
+static bool is_lower_pressed = false;
+static bool is_upper_pressed = false;
+static bool is_ime_locked = false;
+
 enum planck_layers {
     _QWERTY,
     _LOWER,
@@ -11,6 +15,10 @@ enum planck_layers {
 
 enum planck_keycodes {
     ESC_ENG = SAFE_RANGE,
+    LOWER,
+    RAISE,
+    IME_LOCK,
+    IME_UNLOCK
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -19,28 +27,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    TT(_MOUSE), KC_LALT, KC_LGUI, KC_LNG2,  MO(_LOWER),  XXXXXXX, KC_SPC,  MO(_RAISE), KC_LNG1,  KC_RGUI, KC_RALT, KC_RCTL
+    XXXXXXX, TT(_MOUSE), KC_LALT, KC_LGUI,  LOWER,  XXXXXXX, KC_SPC,  RAISE,  KC_RGUI, KC_RALT, KC_RCTL, XXXXXXX
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
      KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
     KC_LCTL, XXXXXXX, SGUI(KC_LEFT), KC_ESC, ESC_ENG, KC_CAPS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, XXXXXXX, KC_PLUS,
     KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, TT(_NUMPAD), XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_RGUI, KC_RALT,
-    _______, _______, _______, _______, _______, _______, KC_ENT, MO(_ADJUST), _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, KC_ENT, RAISE, _______, _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_CIRC,  KC_DLR, KC_UNDS,
     KC_LCTL, KC_MINS,  KC_EQL, KC_PLUS, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_BSLS, KC_SLSH, XXXXXXX, XXXXXXX,
     KC_LSFT, XXXXXXX, XXXXXXX,  KC_GRV,   KC_LT, KC_LBRC, KC_RBRC,   KC_GT, KC_PIPE, XXXXXXX, XXXXXXX, XXXXXXX,
-    _______, _______, _______, _______, MO(_ADJUST), _______, KC_DEL, _______, _______,  _______, _______, _______
+    _______, _______, _______, _______, LOWER, _______, KC_DEL, _______, _______,  _______, _______, _______
 ),
 
 
 [_ADJUST] = LAYOUT_planck_grid(
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,     KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
     KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, KC_WAKE, KC_SLEP, KC_WBAK, KC_WSCH, KC_WHOM, KC_WFWD, KC_WREF,  KC_PSCR,
-    KC_LSFT, XXXXXXX, XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, KC_INS,
+    KC_LSFT, XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, IME_LOCK, IME_UNLOCK, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, KC_INS,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
@@ -73,6 +81,53 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         return false;
         break;
+    case LOWER:
+        if (record->event.pressed) {
+            layer_on(_LOWER);
+            is_lower_pressed = true;
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        } else {
+            layer_off(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            if (is_lower_pressed){
+                tap_code(KC_LNG2);
+            }
+            is_lower_pressed = false;
+        }
+        return false;
+        break;
+    case RAISE:
+        if (record->event.pressed) {
+            layer_on(_RAISE);
+            is_upper_pressed = true;
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        } else {
+            layer_off(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            if(is_upper_pressed && !is_ime_locked){
+                tap_code(KC_LNG1);
+            }
+            is_upper_pressed = false;
+        }
+        return false;
+        break;
+    case IME_LOCK:
+        if (record->event.pressed) {
+            is_ime_locked = true;
+        }
+        return false;
+        break;
+    case IME_UNLOCK:
+        if (record->event.pressed) {
+            is_ime_locked = false;
+        }
+        return false;
+        break;
+    default:
+        if (record->event.pressed) {
+            is_lower_pressed = false;
+            is_upper_pressed = false;
+        }
     }
     return true;
 }
