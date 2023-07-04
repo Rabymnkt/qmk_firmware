1b68b542234c67b894b64307a8aa315f689a9e76
Ryoma Minakata <rabyssm@gmail.com>
2023-07-05 00:44:48 +0900

    update keymap.c

--- keyboards/planck/rev7/keymaps/rabymnkt/keymap.c
+++ keyboards/planck/rev7/keymaps/rabymnkt/keymap.c
@@ -25,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
      KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
     KC_LCTL, XXXXXXX, SGUI(KC_LEFT), KC_ESC, ESC_ENG, KC_CAPS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, XXXXXXX, KC_PLUS,
-    KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, TT(_NUMPAD), XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, XXXXXXX,
-    _______, _______, _______, _______, _______, _______, KC_ENT, MO(_ADJUST), _______,  KC_VOLD, KC_VOLU, KC_MUTE
+    KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, TT(_NUMPAD), XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_RGUI, KC_RALT,
+    _______, _______, _______, _______, _______, _______, KC_ENT, MO(_ADJUST), _______, _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT_planck_grid(
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_planck_grid(
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,     KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
     KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, KC_WAKE, KC_SLEP, KC_WBAK, KC_WSCH, KC_WHOM, KC_WFWD, KC_WREF,  KC_PSCR,
-    KC_LSFT, XXXXXXX, XXXXXXX, BL_DOWN, BL_TOGG, BL_UP, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, KC_INS,
+    KC_LSFT, XXXXXXX, XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, KC_INS,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
@@ -53,9 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_NUMPAD] = LAYOUT_planck_grid(
      KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_P7,   KC_P8,   KC_P9, KC_BSPC, KC_DEL,
-    KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_P4,   KC_P5,   KC_P6, KC_ASTR,  KC_PERC,
-    KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, TT(_NUMPAD), XXXXXXX, XXXXXXX,  KC_P1,   KC_P2,   KC_P3, KC_PLUS, KC_MINS,
-    _______, _______, _______, _______, _______, _______, XXXXXXX ,     KC_P0, KC_PDOT,  KC_ENT, KC_EQL, KC_NUM
+    KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_P4,   KC_P5,   KC_P6, KC_PAST, KC_PSLS,
+    KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, TT(_NUMPAD), XXXXXXX, XXXXXXX,  KC_P1,   KC_P2,   KC_P3, KC_PPLS, KC_PMNS,
+    _______, _______, _______, _______, _______, _______, XXXXXXX ,     KC_P0, KC_PDOT,  KC_ENT, KC_PEQL, KC_NUM
 )
 };
 
