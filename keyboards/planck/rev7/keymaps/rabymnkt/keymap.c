fb04624dd98c9631c0296c0241b68121fc50937d
Ryoma Minakata <rabyssm@gmail.com>
2023-07-15 17:50:18 +0900

    fix planck/rev7 keymap.c

--- keyboards/planck/rev7/keymaps/rabymnkt/keymap.c
+++ keyboards/planck/rev7/keymaps/rabymnkt/keymap.c
@@ -24,32 +24,32 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,  KC_Q,       KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_MINS,
-    KC_LCTL, KC_A,       KC_S,    KC_D,    KC_F,  KC_G,    KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,       KC_X,    KC_C,    KC_V,  KC_B,    KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    XXXXXXX, TT(_MOUSE), KC_LALT, KC_LGUI, LOWER, KC_BSPC, KC_SPC, RAISE, KC_RGUI, KC_RALT, KC_RCTL, XXXXXXX
+    KC_TAB,  KC_Q,       KC_W,    KC_E,    KC_R,       KC_T,    KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,    KC_MINS,
+    KC_LCTL, KC_A,       KC_S,    KC_D,    KC_F,       KC_G,    KC_H,   KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,       KC_X,    KC_C,    KC_V,       KC_B,    KC_N,   KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    XXXXXXX, TT(_MOUSE), KC_LALT, KC_LGUI, MO(_LOWER), KC_BSPC, KC_SPC, MO(_RAISE), KC_LNG1, KC_LNG2, KC_RCTL, XXXXXXX
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
-     KC_TAB, KC_1,    KC_2,          KC_3,    KC_4,        KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_UNDS,
-    KC_LCTL, XXXXXXX, SGUI(KC_LEFT), KC_ESC,  ESC_ENG,     KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_RCTL,
-    KC_LSFT, XXXXXXX, XXXXXXX,       XXXXXXX, TT(_NUMPAD), XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_RGUI, KC_RALT,
-    _______, _______, _______,       _______, _______,     _______, KC_ENT,  RAISE,   _______, _______, _______, _______
+     KC_TAB, KC_1,    KC_2,          KC_3,    KC_4,        KC_5,    KC_6,    KC_7,        KC_8,    KC_9,    KC_0,    KC_UNDS,
+    KC_LCTL, XXXXXXX, SGUI(KC_LEFT), KC_ESC,  ESC_ENG,     KC_CAPS, KC_LEFT, KC_DOWN,     KC_UP,   KC_RGHT, XXXXXXX, KC_RCTL,
+    KC_LSFT, XXXXXXX, XXXXXXX,       XXXXXXX, TT(_NUMPAD), XXXXXXX, KC_HOME, KC_PGDN,     KC_PGUP, KC_END,  KC_RGUI, KC_RALT,
+    _______, _______, _______,       _______, _______,     _______, KC_ENT,  MO(_ADJUST), _______, _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_CIRC, KC_DLR,  KC_UNDS,
-    KC_LCTL, KC_MINS, KC_EQL,  KC_PLUS, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_BSLS, KC_SLSH, XXXXXXX, KC_RCTL,
-    KC_LSFT, XXXXXXX, XXXXXXX, KC_GRV,  KC_LT,   KC_LBRC, KC_RBRC, KC_GT,   KC_PIPE, XXXXXXX, KC_RGUI, KC_RALT,
-    _______, _______, _______, _______, LOWER,   KC_DEL,  _______, _______, _______, _______, _______, _______
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,      KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_CIRC, KC_DLR,  KC_UNDS,
+    KC_LCTL, KC_MINS, KC_EQL,  KC_PLUS, KC_LCBR,     KC_LPRN, KC_RPRN, KC_RCBR, KC_BSLS, KC_SLSH, XXXXXXX, KC_RCTL,
+    KC_LSFT, XXXXXXX, XXXXXXX, KC_GRV,  KC_LT,       KC_LBRC, KC_RBRC, KC_GT,   KC_PIPE, XXXXXXX, KC_RGUI, KC_RALT,
+    _______, _______, _______, _______, MO(_ADJUST), KC_DEL,  _______, _______, _______, _______, _______, _______
 ),
 
 
 [_ADJUST] = LAYOUT_planck_grid(
-      KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,      KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    KC_LCTL, KC_PSCR, XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU,  KC_WBAK,    KC_WSCH, KC_WHOM, KC_WFWD, KC_WREF, _______,
-    KC_LSFT, KC_INS,  XXXXXXX, XXXXXXX, XXXXXXX, IME_LOCK, IME_UNLOCK, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
-    _______, _______, _______, _______, _______, _______,  _______,    _______, _______, _______, _______, _______
+      KC_F1, KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    KC_LCTL, KC_PSCR, XXXXXXX, KC_VOLD,  KC_MUTE, KC_VOLU,    KC_WBAK, KC_WSCH, KC_WHOM, KC_WFWD, KC_WREF, _______,
+    KC_LSFT, KC_INS,  XXXXXXX, IME_LOCK, KC_LNG2, IME_UNLOCK, KC_MPRV, KC_LNG1, KC_MPLY, KC_MNXT, _______, _______,
+    _______, _______, _______, _______,  _______, _______,    _______, _______, _______, _______, _______, _______
 ),
 
 [_MOUSE] = LAYOUT_planck_grid(
