b000fc0514c4dab1a9e28d9b60e94d2abdba2c53
Ryoma Minakata <rabyssm@gmail.com>
2024-12-21 01:30:38 +0900

    revert: previous commit

--- keyboards/planck/rev7/config.h
+++ keyboards/planck/rev7/config.h
@@ -17,6 +17,9 @@
 
 #pragma once
 
+#define DIP_SWITCH_PINS \
+    { B14, A15, A0, B9 }
+
 #define MUSIC_MAP
 #undef AUDIO_VOICES
 #undef AUDIO_PIN
@@ -27,8 +30,10 @@
 #define WS2812_PWM_DRIVER PWMD2
 #define WS2812_PWM_CHANNEL 2
 #define WS2812_PWM_PAL_MODE 1
-#define WS2812_PWM_DMA_STREAM STM32_DMA1_STREAM2
-#define WS2812_PWM_DMA_CHANNEL 2
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM2
+#define WS2812_DMA_CHANNEL 2
+
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
 
 /*
  * Feature disable options
